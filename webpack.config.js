fc436018cf65719cda877b731d0768b8b8006d57
zzc <370531378@qq.com>
2019-02-20 20:35:54 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode:'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                ]
+            }
+
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./dist"),
+        compress: true,
+        port: 9000
+    }
+};
